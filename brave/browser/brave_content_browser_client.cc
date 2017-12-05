660dac8548d50fc090150372a8181a08a8734f83
Sergio Villar Senin <svillar@igalia.com>
2017-12-05 17:11:34 +0100

    Create service metrics on BraveContentBrowserClient
    The url_keyed_metrics capability was moved from browser to an embedded
    service. This means that the ContentBrowserClients should from now on,
    create the metrics service, by binding directly to it.
    
    This was causing errors during startup
    "service_manager_connection_impl.cc(289)] Can't create service metrics".
    
    See https://crrev.com/514656

--- brave/browser/brave_content_browser_client.cc
+++ brave/browser/brave_content_browser_client.cc
@@ -62,6 +62,8 @@
 #include "muon/app/muon_crash_reporter_client.h"
 #include "net/base/filename_util.h"
 #include "services/data_decoder/public/interfaces/constants.mojom.h"
+#include "services/metrics/metrics_mojo_service.h"
+#include "services/metrics/public/interfaces/constants.mojom.h"
 #include "services/proxy_resolver/proxy_resolver_service.h"
 #include "services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
@@ -320,11 +322,16 @@ void BraveContentBrowserClient::RegisterInProcessServices(
     info.factory = base::Bind(&ChromeService::Create);
     services->insert(std::make_pair(chrome::mojom::kServiceName, info));
   }
-  service_manager::EmbeddedServiceInfo info;
-  info.factory =
+  {
+    service_manager::EmbeddedServiceInfo info;
+    info.factory =
       base::Bind(&proxy_resolver::ProxyResolverService::CreateService);
-  services->insert(
-      std::make_pair(proxy_resolver::mojom::kProxyResolverServiceName, info));
+    services->insert(
+        std::make_pair(proxy_resolver::mojom::kProxyResolverServiceName, info));
+  }
+  service_manager::EmbeddedServiceInfo info;
+  info.factory = base::Bind(&metrics::CreateMetricsService);
+  services->emplace(metrics::mojom::kMetricsServiceName, info);
 }
 
 void BraveContentBrowserClient::RegisterOutOfProcessServices(
